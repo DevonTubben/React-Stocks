b0db7e97b0d3a8f952925f16f73f674383f27753
Devon Tubben <tubbend1@msu.edu>
2021-07-11 11:07:22 -0400

    done.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,33 +1,29 @@
 import React from 'react';
 
-const SearchBar = () => {
+const SearchBar = (props) => {
   return (
     <div>
 
       <strong>Sort by:</strong>
       <label>
-        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
+        <input type="radio" value="Alphabetically" checked={null} onChange={props.handleAChange} />
         Alphabetically
       </label>
       <label>
-        <input type="radio" value="Price" checked={null} onChange={null}/>
+        <input type="radio" value="Price" checked={null} onChange={props.handleBChange} />
         Price
       </label>
-      <br/>
+      <br />
 
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select onChange={(event) => { props.handleFilter(event) }}>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
         </select>
       </label>
-
-
     </div>
   );
 }
-
-
 export default SearchBar;
